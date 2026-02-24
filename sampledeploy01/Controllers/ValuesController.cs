ca910fedcb368b7a0fba0a77cb965024dfedb17e
ganes <ganes@DESKTOP-N99PNFH>
2026-02-24 10:45:13 -0500

    Add project files.

---
+++ sampledeploy01/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sampledeploy01.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("/test")]
+        public async Task<string> TestTask()
+        {
+            return await Task.FromResult("Test call");
+        }
+    }
+}
---
+++ sampledeploy01/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Register Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
